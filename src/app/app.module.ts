fb3d11650e8ee0ecad3aa35b5fed88b833ebc09a
Axe <gustavo.peralta@gruposolutio.com>
2019-01-28 11:19:25 +0100

    add dropdwon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import {PopoverModule } from 'ngx-bootstrap/popover'
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AccordionModule.forRoot(),
     PopoverModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
